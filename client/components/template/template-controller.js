96be0f513a47bf82de8f409a920ec8f1ebe789fa
Fabio Augusto <fa-pouza@hotmail.com>
2017-07-04 17:35:02 -0300

    Criado json para geração do HTML; Criado promisses para salvar as informações de maneira ordenada no .txt

--- client/components/template/template-controller.js
+++ client/components/template/template-controller.js
@@ -3,29 +3,69 @@ angular.module('PackageGenerator')
     .controller('TemplateController', ['$scope', function ($scope) {
 
         $scope.questions = [
-            { type: 'input', inputType: 'text', placeholder: 'nome', value: 'nomePadrao' },
-            { type: 'input', inputType: 'text', placeholder: 'senha', },
-            { type: 'input', inputType: 'checkbox', label: { description: 'Deseja gerar um TEXTÃO?' }, value: true },
+    {"label": {"description": "CÓDIGO DO PROJETO/INCIDENTE:"}},
+    {"type": "input", "inputType": "text"},
+    {"label": {"description": "DESCRIÇÃO DA MUDANÇA:"}},
+    {"type": "input", "inputType": "text"},
+    {"label": {"description": "SERVIÇOS AFETADOS:"}},
+    {"type": "input", "inputType": "text"},
+    {"label": {"description": "Pré-Requisito"}},
+    {"label": {"description": "N/A"}},
+    {
+        "id": "dpcd", "label": {"description": "1.Existe projeto aberto no DPCD"}, subQuestions: [
             {
-                id: 'versao-was', label: { description: 'Versão do WebSphere:' }, subQuestions: [
-                    {
-                        id: 'versao-was6.1', type: 'input', inputType: 'radio', name: 'versao-was',
-                        label: { for: 'versao-was6.1', description: '6.1' }, ngValue: '6.1',
-                    },
-                    {
-                        id: 'versao-was8.5', type: 'input', inputType: 'radio', name: 'versao-was',
-                        label: { for: 'versao-was8.5', description: '8.5' }, ngValue: '8.5',
-                    },
-                ]
+                "id": "naoDpcd", "type": "input", "inputType": "radio", "name": "Dpcd",
+                "label": {"for": "naoDpcd", "description": "Não"}, "ngValue": "Não"
             },
-        ];
+            {
+                "id": "naoDpcd", "type": "input", "inputType": "radio", "name": "Dpcd",
+                "label": {"for": "naoDpcd", "description": "Sim, Informe o numero do projeto"}, "ngValue": "Não"
+            },
+            {"type": "input", "inputType": "text"},
+        ]
+    },
+    {
+        "id": "tratamentoAgencia", "label": {"description": "2.Tratamento nova faixa de código de junção. O aplicativo faz tratamento de codigo de junção de agência no range 1 a 3999"}, subQuestions: [
+            {
+                "id": "tratamentoNao", "type": "input", "inputType": "radio", "name": "tratamento",
+                "label": {"for": "naoTratamento", "description": "Não"}, "ngValue": "Não"
+            },
+            {
+                "id": "tratamentoSimFoi", "type": "input", "inputType": "radio", "name": "tratamento",
+                "label": {"for": "simFoiTratado", "description": "Sim, Já foi alterado para tratar agencia em quanquer range"}, "ngValue": "Sim, Já foi alterado para tratar agencia em quanquer range"
+            },
+            {
+                "id": "tratamentoSimNao", "type": "input", "inputType": "radio", "name": "tratamento",
+                "label": {"for": "simNaoTratado", "description": "Sim, Não foi alterado para tratar agencia em quanquer range"}, "ngValue": "Sim, Não foi alterado para tratar agencia em quanquer range"
+            },   
+        ]
+    },
+    {"label": {"description": "3.Ambientes afetados com a mudança"}},
+    { "type": "input", "inputType": "checkbox", "label": { "description": "Internet - Servidores" }, "value": true },
+    {"label": {"description": "Servidores IBMHttpServer:"}},
+    {"type": "textarea", "name": "servidores", "id": "servidores", "rows": "4", "cols": "50"},
+    {"label": {"description": "Servidores Fujitsu WebSphere WAS 6.1 IB CTI"}},
+    {"type": "textarea", "name": "servidores", "id": "servidores", "rows": "4", "cols": "50"},
+    {"label": {"description": "Servidores Windows criptografia:"}},
+    {"type": "input", "inputType": "text"},
+    {"label": {"description": "Servidores Windows transacional:"}},
+    {"type": "input", "inputType": "text"},
+    {"label": {"description": "Servidores FAST ESP:"}},
+    {"type": "input", "inputType": "text"},
+    { "type": "input", "inputType": "checkbox", "label": { "description": "Corporativa - Seridores:" }, "value": true },
+    { "type": "input", "inputType": "checkbox", "label": { "description": "Extranet - Servidores" }, "value": true },
+    { "type": "input", "inputType": "checkbox", "label": { "description": "Extracorp - Servidores" }, "value": true },
+    { "type": "input", "inputType": "checkbox", "label": { "description": "Intranet - Servidores:" }, "value": true },
+    { "type": "input", "inputType": "checkbox", "label": { "description": "Departamental - Servidores" }, "value": true },
+    { "type": "input", "inputType": "checkbox", "label": { "description": "Outro. Especifique" }, "value": true },
+];
 
         $scope.gerar = function () {
             converser($scope);
 
-            // $scope.questions.forEach(function (question) {
-            //     console.log(`question: ${question.id || question.$$hashKey}. value: ${question.value}`);
-            // }, this);
+            //  $scope.questions.forEach(function (question) {
+            //      console.log(`question: ${question.id || question.$$hashKey}. value: ${question.value}`);
+            //  }, this);
         };
 
     }]);
--- scripts/converser.js
+++ scripts/converser.js
@@ -1,9 +1,15 @@
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require("fs"));
+var promiseDoArquivo;
+
 function converser($scope) {
     $scope.questions.forEach(function (question) {
         if (question.inputType === 'text') {
             console.log(`question: ${question.id || question.$$hashKey}. value: ${question.value}`);
+            save(question);
         }
         if (question.inputType === 'checkbox' && question.value === true) {
+            oi(question);
             console.log(question.label.description);
             console.log('( X )');
         }
@@ -20,4 +26,28 @@ function converser($scope) {
 
 function separator() {
     console.log('--------------------------------------------');
+}
+
+function save(question) {
+    var promiseDoAppend = fs.appendFileAsync('./TXT/teste.txt', `Nome: ${question.value} \n`);
+    if (!promiseDoArquivo) {
+        promiseDoArquivo = promiseDoAppend;
+    }
+    else {
+        promiseDoArquivo
+            .then(() => {
+                console.log('The "data to append" was appended to file!');
+                return promiseDoPrimeiroAppend;
+            })
+            .catch((err) => {
+                
+            })
+    }
+}
+
+function oi(question) {
+    fs.appendFile('./TXT/teste.txt', `${question.label.description} ( X ) \n`, (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+    });
 }
